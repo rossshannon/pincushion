f5ee50d98afa42932d75bac5709068dc352f9f83
Ross Shannon <ross.shannon@gmail.com>
2014-04-12 01:19:34 +0100

    Formatting and interaction of tag suggestions.

--- popup.js
+++ popup.js
@@ -223,7 +223,7 @@ function show_suggested_tags(tag_suggestions) {
     var escaped = pin_escape(suggested_tag);
     var cooked  = pin_cook(suggested_tag);
     var suggested_tag = '<button type="button" class="suggested_tag" onclick="add_tag(\''  +
-                escaped + '\'); $(this).hide(); return false;">' + cooked + '</button>';
+                escaped + '\'); $(this).hide(200); return false;">' + cooked + '</button>';
     suggested_tags.push(suggested_tag);
   }
 
