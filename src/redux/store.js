2fc1b26cfe7b30996234751c8da824b74c2d7c1e
rossshannon <rossshannon@users.noreply.github.com>
2025-11-14 08:50:42 +0000

    deploy: 87a746ac35dafb50301b2535d841f0a9f469989c

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
-
-// Create Redux store with combined reducers
-const store = configureStore({
-  reducer: rootReducer
-});
-
-export default store;
